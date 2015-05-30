2b9cadf31ff266f23da77fc75601a4e779bddf83
marfee <kouhot6210@gmail.com>
2015-05-30 15:38:16 +0900

    Twitter Client
    This is a TwitterClient that has some functions such as  “tweet” ,
    “account”and display timeline.

---
+++ TwitterClient02/TimeLineTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  TimeLineTableViewController.h
+//  TwitterClient02
+//
+//  Created by Fukazawa kota on 2015/05/27.
+//  Copyright (c) 2015年 Kota Fukazawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
+#import "timeLineCell.h"
+
+@interface TimeLineTableViewController : UITableViewController
+@property (nonatomic, copy) NSString *identifier;
+
+@end
